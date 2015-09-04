7d37785b0a50b0311614f05b60506f3293d33127
Victor <victor.enaud@hotmail.fr>
2015-09-04 17:05:19 +0200

    Suite TP 1 (Exercice 1 + 2)

--- TP_1/ProjetTP1/src/projettp1/ProjetTP1.java
+++ TP_1/ProjetTP1/src/projettp1/ProjetTP1.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projettp1;
 
 /**
  *
- * @author Victor
+ * @author Jean-Marie Normand (jean-marie.normand@ec-nantes.fr)
  */
 public class ProjetTP1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int i = 0;
+        System.out.println(
+        "Début du programme !");
+                      // premiere partie du programme
+                      i *= 2;
+        i = i + 1;
+        System.out.println("La variable i vaut : " + i);
+        // deuxieme partie du programme
+        i += 2;
+        i = (i / 2) * 2;
+        System.out.println("Fin du programme, je pense que i vaut 3");
+        System.out.println("Le résultat final est : " + i);
+
+        // troisieme partie : bissextile
+        int annee = 1988;
+        if (((annee / 4 == 0) && (annee / 100 != 0)) || (annee / 400 == 0)) {
+            System.out.println(annee + " est une annee bissextile");
+        } else {
+            System.out.println(annee + " n'est pas bissextile");
+        }
+
     }
-    
+
 }
