7fce53d6b83f8c6c3a62c31d746539a30934c8b0
Victor <victor.enaud@hotmail.fr>
2015-09-18 15:10:14 +0200

    Modifications diverses
    Début de la classe world
    Modification de l'indentation
    Modification des commentaires de Personnage et Monstre pour le deplace

--- TP_2/ProjetTP/src/org/centrale/projet/objet/Paysan.java
+++ TP_2/ProjetTP/src/org/centrale/projet/objet/Paysan.java
@@ -13,46 +13,46 @@ public class Paysan extends Personnage {
     
  // Méthodes
     // Constructeurs
-    public Paysan() {
-        // Constructeur par défaut
-        this.ptVie = 80;
-        this.ptMana = 50;
-        this.pourcentageAtt = 30;
-        this.pourcentagePar = 30;
-        this.pourcentageMag = 10;
-        this.pourcentageResistMag = 20;
-        this.degAtt = 20;
-        this.degMag = 10;
-        this.distAttMax = 3;
-        this.pos = new Point2D(0,0);
-    }
-    
-    public Paysan(Paysan pequenot) {
-        // Constructeur par défaut
-        this.ptVie = pequenot.ptVie;
-        this.ptMana = pequenot.ptMana;
-        this.pourcentageAtt = pequenot.pourcentageAtt;
-        this.pourcentagePar = pequenot.pourcentagePar;
-        this.pourcentageMag = pequenot.pourcentageMag;
-        this.pourcentageResistMag = pequenot.pourcentageResistMag;
-        this.degAtt = pequenot.degAtt;
-        this.degMag = pequenot.degMag;
-        this.distAttMax = pequenot.distAttMax;
-        this.pos = pequenot.pos;
-    }
-    
-    public Paysan(int pV, int pM, int pA, int pP, int pctM, int rM, int dA, int dM, int distMax, int nbFleches, Point2D pos) {
-        // Constructeur de personnage customisé
-        this.ptVie = pV;
-        this.ptMana = pM;
-        this.pourcentageAtt = pA;
-        this.pourcentagePar = pP;
-        this.pourcentageMag = pctM;
-        this.pourcentageResistMag = rM;
-        this.degAtt = dA;
-        this.degMag = dM;
-        this.distAttMax = distMax;
-        this.pos = pos;
-    }   
+        public Paysan() {
+            // Constructeur par défaut
+            this.ptVie = 80;
+            this.ptMana = 50;
+            this.pourcentageAtt = 30;
+            this.pourcentagePar = 30;
+            this.pourcentageMag = 10;
+            this.pourcentageResistMag = 20;
+            this.degAtt = 20;
+            this.degMag = 10;
+            this.distAttMax = 3;
+            this.pos = new Point2D(0,0);
+        }
+
+        public Paysan(Paysan pequenot) {
+            // Constructeur par défaut
+            this.ptVie = pequenot.ptVie;
+            this.ptMana = pequenot.ptMana;
+            this.pourcentageAtt = pequenot.pourcentageAtt;
+            this.pourcentagePar = pequenot.pourcentagePar;
+            this.pourcentageMag = pequenot.pourcentageMag;
+            this.pourcentageResistMag = pequenot.pourcentageResistMag;
+            this.degAtt = pequenot.degAtt;
+            this.degMag = pequenot.degMag;
+            this.distAttMax = pequenot.distAttMax;
+            this.pos = pequenot.pos;
+        }
+
+        public Paysan(int pV, int pM, int pA, int pP, int pctM, int rM, int dA, int dM, int distMax, int nbFleches, Point2D pos) {
+            // Constructeur de personnage customisé
+            this.ptVie = pV;
+            this.ptMana = pM;
+            this.pourcentageAtt = pA;
+            this.pourcentagePar = pP;
+            this.pourcentageMag = pctM;
+            this.pourcentageResistMag = rM;
+            this.degAtt = dA;
+            this.degMag = dM;
+            this.distAttMax = distMax;
+            this.pos = pos;
+        }   
     
 }
--- TP_2/ProjetTP/src/org/centrale/projet/objet/Personnage.java
+++ TP_2/ProjetTP/src/org/centrale/projet/objet/Personnage.java
@@ -15,166 +15,179 @@ import java.util.Random;
 public class Personnage {
 
     // Attributs
-    protected int ptVie;
-    protected int ptMana;
-    protected int pourcentageAtt;
-    protected int pourcentagePar;
-    protected int pourcentageMag;
-    protected int pourcentageResistMag;
-    protected int degAtt;
-    protected int degMag;
-    protected int distAttMax;
-    protected Point2D pos;
+        protected String nom;
+        protected int ptVie;
+        protected int ptMana;
+        protected int pourcentageAtt;
+        protected int pourcentagePar;
+        protected int pourcentageMag;
+        protected int pourcentageResistMag;
+        protected int degAtt;
+        protected int degMag;
+        protected int distAttMax;
+        protected Point2D pos;
 
     // Constructeurs
-    public Personnage() {
-        // Constructeur de personnage générique
-        this.ptVie = 100;
-        this.ptMana = 100;
-        this.pourcentageAtt = 50;
-        this.pourcentagePar = 50;
-        this.pourcentageMag = 50;
-        this.pourcentageResistMag = 50;
-        this.degAtt = 20;
-        this.degMag = 20;
-        this.distAttMax = 3;
-        this.pos = new Point2D(0, 0);
-    }
+        public Personnage() {
+            // Constructeur de personnage générique
+            this.nom = "JYM";
+            this.ptVie = 100;
+            this.ptMana = 100;
+            this.pourcentageAtt = 50;
+            this.pourcentagePar = 50;
+            this.pourcentageMag = 50;
+            this.pourcentageResistMag = 50;
+            this.degAtt = 20;
+            this.degMag = 20;
+            this.distAttMax = 3;
+            this.pos = new Point2D(0, 0);
+        }
 
-    public Personnage(Personnage perso) {
-        // Constructeur de personnage par copie
-        this.ptVie = perso.ptVie;
-        this.ptMana = perso.ptMana;
-        this.pourcentageAtt = perso.pourcentageAtt;
-        this.pourcentagePar = perso.pourcentagePar;
-        this.pourcentageMag = perso.pourcentageMag;
-        this.pourcentageResistMag = perso.pourcentageResistMag;
-        this.degAtt = perso.degAtt;
-        this.degMag = perso.degMag;
-        this.distAttMax = perso.distAttMax;
-        this.pos = perso.pos;
-    }
+        public Personnage(Personnage perso) {
+            // Constructeur de personnage par copie
+            this.nom = perso.nom;
+            this.ptVie = perso.ptVie;
+            this.ptMana = perso.ptMana;
+            this.pourcentageAtt = perso.pourcentageAtt;
+            this.pourcentagePar = perso.pourcentagePar;
+            this.pourcentageMag = perso.pourcentageMag;
+            this.pourcentageResistMag = perso.pourcentageResistMag;
+            this.degAtt = perso.degAtt;
+            this.degMag = perso.degMag;
+            this.distAttMax = perso.distAttMax;
+            this.pos = perso.pos;
+        }
 
-    public Personnage(int pV, int pM, int pA, int pP, int pctM, int rM, int dA, int dM, int distMax, Point2D pos) {
-        // Constructeur de personnage customisé
-        this.ptVie = pV;
-        this.ptMana = pM;
-        this.pourcentageAtt = pA;
-        this.pourcentagePar = pP;
-        this.pourcentageMag = pctM;
-        this.pourcentageResistMag = rM;
-        this.degAtt = dA;
-        this.degMag = dM;
-        this.distAttMax = distMax;
-        this.pos = pos;
-    }
+        public Personnage(String nom, int pV, int pM, int pA, int pP, int pctM, int rM, int dA, int dM, int distMax, Point2D pos) {
+            // Constructeur de personnage customisé
+            this.nom = nom;
+            this.ptVie = pV;
+            this.ptMana = pM;
+            this.pourcentageAtt = pA;
+            this.pourcentagePar = pP;
+            this.pourcentageMag = pctM;
+            this.pourcentageResistMag = rM;
+            this.degAtt = dA;
+            this.degMag = dM;
+            this.distAttMax = distMax;
+            this.pos = pos;
+        }
 
     // Méthodes
-    // Affectation
-    public void setPtVie(int pV) {
-        this.ptVie = pV;
-    }
+        // Affectation
 
-    public void setPtMana(int pM) {
-        this.ptMana = pM;
-    }
+            public void setNom(String nom) {
+                this.nom = nom;
+            }
 
-    public void setPourcentageAtt(int pA) {
-        this.pourcentageAtt = pA;
-    }
+            public void setPtVie(int pV) {
+                this.ptVie = pV;
+            }
 
-    public void setPourcentagePar(int pP) {
-        this.pourcentagePar = pP;
-    }
+            public void setPtMana(int pM) {
+                this.ptMana = pM;
+            }
 
-    public void setPourcentageMag(int pctM) {
-        this.pourcentageMag = pctM;
-    }
+            public void setPourcentageAtt(int pA) {
+                this.pourcentageAtt = pA;
+            }
 
-    public void setPourcentageResistMag(int rM) {
-        this.pourcentageResistMag = rM;
-    }
+            public void setPourcentagePar(int pP) {
+                this.pourcentagePar = pP;
+            }
 
-    public void setDegAtt(int dA) {
-        this.degAtt = dA;
-    }
+            public void setPourcentageMag(int pctM) {
+                this.pourcentageMag = pctM;
+            }
 
-    public void setDegMag(int dM) {
-        this.degMag = dM;
-    }
+            public void setPourcentageResistMag(int rM) {
+                this.pourcentageResistMag = rM;
+            }
 
-    public void setDistAttMax(int distMax) {
-        this.distAttMax = distMax;
-    }
+            public void setDegAtt(int dA) {
+                this.degAtt = dA;
+            }
 
-    public void setPos(Point2D pos) {
-        this.pos = pos;
-    }
+            public void setDegMag(int dM) {
+                this.degMag = dM;
+            }
 
-        // Lecture
-    public int getPtVie() {
-        return this.ptVie;
-    }
+            public void setDistAttMax(int distMax) {
+                this.distAttMax = distMax;
+            }
 
-    public int getPtMana() {
-        return this.ptMana;
-    }
+            public void setPos(Point2D pos) {
+                this.pos = pos;
+            }
 
-    public int getPourcentageAtt() {
-        return this.pourcentageAtt;
-    }
+            // Lecture
 
-    public int getPourcentagePar() {
-        return this.pourcentagePar;
-    }
+            public String getNom() {
+                return nom;
+            }
 
-    public int getPourcentageMag() {
-        return this.pourcentageMag;
-    }
+            public int getPtVie() {
+                return this.ptVie;
+            }
 
-    public int getPourcentageResistMag() {
-        return this.pourcentageResistMag;
-    }
+            public int getPtMana() {
+                return this.ptMana;
+            }
 
-    public int getDegAtt() {
-        return this.degAtt;
-    }
+            public int getPourcentageAtt() {
+                return this.pourcentageAtt;
+            }
 
-    public int getDegMag() {
-        return this.degMag;
-    }
+            public int getPourcentagePar() {
+                return this.pourcentagePar;
+            }
 
-    public int getDistAttMax() {
-        return this.distAttMax;
-    }
+            public int getPourcentageMag() {
+                return this.pourcentageMag;
+            }
 
-    public Point2D getPos() {
-        return this.pos;
-    }
+            public int getPourcentageResistMag() {
+                return this.pourcentageResistMag;
+            }
+
+            public int getDegAtt() {
+                return this.degAtt;
+            }
+
+            public int getDegMag() {
+                return this.degMag;
+            }
+
+            public int getDistAttMax() {
+                return this.distAttMax;
+            }
+
+            public Point2D getPos() {
+                return this.pos;
+            }
 
         // Autres
-    // add methode deplace
     
-    public void deplace() {
-        // deplace de façon aléatoire un personnage sur la carte
-        // génère un nombre aléatoire entre 1 et 4, et selon le tirage, déplace le personnage
-        // 1 = N, 2 = E, 3 = S, 4 = O
-        Random generateurAleatoire = new Random();
-        int entierAlea = generateurAleatoire.nextInt(1);
-        if (entierAlea == 1){
-            this.pos.y = this.pos.y + 1;
-        }else if (entierAlea == 2){
-            this.pos.x = this.pos.x - 1;
-        }else if (entierAlea == 3){
-            this.pos.y = this.pos.y - 1;
-        }else{
-            this.pos.x = this.pos.x + 1;
-        }
-        
-    }
-    
-    public void affiche() {
-        System.out.println("Les coordonnées du personnage sont : [" + this.pos.x + ";" + this.pos.y + "]");
-        System.out.println("Il a " + this.ptVie + " PV, " + this.ptMana + " PM, " + this.pourcentageAtt + " PA, " + this.pourcentageMag + " pctM " + this.pourcentagePar + " PM, " + this.pourcentageResistMag + " rM, " + this.degAtt + "dA et " + this.degMag + " dM ");
-    }
+            public void deplace() {
+                // deplace de façon aléatoire un personnage sur la carte
+                // génère un nombre aléatoire entre 1 et 4, et selon le tirage, déplace le personnage
+                // 1 = N, 2 = E, 3 = S, 0 = O
+                Random generateurAleatoire = new Random();
+                int entierAlea = generateurAleatoire.nextInt(1);
+                if (entierAlea == 1){
+                    this.pos.y = this.pos.y + 1;
+                }else if (entierAlea == 2){
+                    this.pos.x = this.pos.x - 1;
+                }else if (entierAlea == 3){
+                    this.pos.y = this.pos.y - 1;
+                }else{
+                    this.pos.x = this.pos.x + 1;
+                }
+
+            }
+
+            public void affiche() {
+                System.out.println("Les coordonnées du personnage sont : [" + this.pos.x + ";" + this.pos.y + "]");
+                System.out.println("Il a " + this.ptVie + " PV, " + this.ptMana + " PM, " + this.pourcentageAtt + " PA, " + this.pourcentageMag + " pctM " + this.pourcentagePar + " PM, " + this.pourcentageResistMag + " rM, " + this.degAtt + "dA et " + this.degMag + " dM ");
+            }
 }
